cc5e31864f99898e1b81e77fd5110fb404801840
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-03-04 21:38:10 +0200

    ES6 basic v12.02

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -3,8 +3,8 @@ export default function createReportObject(employeesList) {
     allEmployees: {
       ...employeesList,
     },
-    getNumberOfDepartments() {
-      return this.allEmployees.length;
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     }
   };
 }
