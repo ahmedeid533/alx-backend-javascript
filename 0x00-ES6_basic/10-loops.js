081abcc0977cfdcec8d71ea4f8c8f6d321991760
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-03-04 22:24:19 +0200

    ES6 basic v10.05

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-	let newArray = [];
-	for (const value of array) {
-		newArray.push(`${appendString}${value}`);
-	}
+  let newArray = [];
+  for (const value of array) {
+    newArray.push(`${appendString}${value}`);
+  }
 
-	return newArray;
+  return newArray;
 }
