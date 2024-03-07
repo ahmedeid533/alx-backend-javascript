48190e76332528d522968dedf62b1d52dafc3dfc
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-03-07 15:06:41 +0200

    alx es6 classes v5.1

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,8 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      this.evacuationWarningMessage();
+    }
     this._sqft = sqft;
   }
 
@@ -7,7 +10,9 @@ export default class Building {
     return this._sqft;
   }
 
+  /* eslint-disable */
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
+  /* eslint-enable */
 }
